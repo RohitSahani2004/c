f0bfbb16df480edcbd45abddccfe24cf70c9fe4f
Rohit <rohit763ne@gmail.com>
2026-06-02 22:47:53 +0530

    Resolved merge conflict

--- tut38.c
+++ tut38.c
@@ -1,1 +1,33 @@
-#
+#include <stdio.h>
+int main(){
+     
+<<<<<<< HEAD
+    int num, a = 0, b =  1, c, i;
+=======
+    int num, a = 0, b = 1 , c, i;
+>>>>>>> bbf6e7b (Save current files)
+    printf("Enter the number of terms : ");
+    scanf("%d", &num);
+    printf("Fibonacci series : ");
+    for(i = 0; i < num; i++){
+<<<<<<< HEAD
+        if(i <= 1){
+=======
+        if(i <= 1 ){
+>>>>>>> bbf6e7b (Save current files)
+            c = i;
+        }else{
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("%d ", c);
+<<<<<<< HEAD
+
+    
+    }
+=======
+    }  
+    
+>>>>>>> bbf6e7b (Save current files)
+}
