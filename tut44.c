1c89d5569bcf6c8c16c36ae8c3dfe8f80d19d7d0
Rohit <rohit763ne@gmail.com>
2026-06-02 23:45:06 +0530

    Added tut44.c

---
+++ tut44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    // WAP to print numbers  1 to 100 and stop 50
+    int i;
+
+    for(i = 1; i <=100; i++){
+        if(i == 51){
+            break;
+        }
+        printf("%d\n", i);
+
+        
+    }
+    return 0;
+  
+}
