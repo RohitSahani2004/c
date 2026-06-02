bb6447fbd3e78da0f24408bfc57dc00bce71c902
Rohit <rohit763ne@gmail.com>
2026-06-02 22:59:46 +0530

    Resolved merge conflict

--- tut40.c
+++ tut40.c
@@ -2,19 +2,17 @@
 int main(){
 
     int num, temp, digit, sum = 0;
-    printf("Enter a number : ");
+    printf("Enter a Number : ");
     scanf("%d", &num);
     temp = num;
-
-    for(; num > 0; num /=  10){
-
-            digit = num % 10;
-            sum += digit * digit* digit;
+    for(; num > 0; num /= 10){
+        digit = num % 10;
+        sum += digit*digit*digit;
     }
-        if(sum == temp){
-            printf(" Armstrong Number");
-        }else
-               printf(" Not an Armstrong Number");
-                   return 0;
+    if(temp == sum){
+        printf("%d is an Armstrong Number", temp);
+    } else {
+        printf("%d is not an Armstrong Number", temp);
     }
-        
+    return 0;
+}
