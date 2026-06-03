625d8608e4703b2dd103ffcfbcb6868260dd6bb9
Rohit <rohit763ne@gmail.com>
2026-06-03 14:37:21 +0530

    Resolved merge conflict

--- tut47.c
+++ tut47.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(){
-
+     // 1st method of typecasting
      int a = 5;
      float b = 54.67;
-     printf("The value of a is %d\n ", (int)b);
+     printf("The value of a is %d\n ", (int)b); // typecasting
+     return 0;
+    // 2nd method of typecasting
+     int a = 5;
+     float b = (float)54/5; // typecasting using (float) before the value to be typecasted
+     printf("The value of a is %f\n ", b);
      return 0;
 
 }
