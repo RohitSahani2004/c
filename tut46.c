da8fb167f98f72199561659a032b5cbc2bc5bb55
Rohit <rohit763ne@gmail.com>
2026-06-03 09:13:56 +0530

    Added tut46.c

---
+++ tut46.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+    int i = 0;
+
+    while(i <= 10){
+        i++;
+        if(i  == 6
+        ){
+            continue;
+        }
+        printf("%d\n", i);
+    }
+}
