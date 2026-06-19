46880bbd9e386974e550a8df92125c7d9820c23e
Rohit <Rohit763ne@gmail.com>
2026-06-19 19:39:57 +0530

    tut66.c

---
+++ tut66.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter a number to print the pyramid : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * (n - i) - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
