d1a8d2803a9ac1106c7db72499216d6f06494b71
Rohit <Rohit763ne@gmail.com>
2026-06-19 19:35:31 +0530

    tut65.c

---
+++ tut65.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter a number to print  reverse pyramid star pattern : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * (n - i)-1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
